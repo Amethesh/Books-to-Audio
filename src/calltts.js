be2eb874a4044340d5f02688fb9c91d17aa883af
Amethesh LGP <ametheshlgp@gmail.com>
2023-04-16 22:02:47 +0530

    Creating calltts

---
+++ src/calltts.js
@@ -0,0 +1,17 @@
+import fs from "fs";
+import path from "path";
+import tts from "./tts.js";
+ 
+//! Using Promises
+const __dirname = path.resolve(); 
+
+export const calltts = async (output_path,API_KEY,voice_id) => {
+
+    for( let i = 1 ; i <= 3; i++){
+
+        await tts(i,i,output_path,API_KEY,voice_id)
+        
+    }
+}
+
+calltts("./Audio/Shadow/Chapter1.mp3", "5b55c001f2b6c48f60f6a8a1f2b1f93c", "TV3QEkEEEOv8GwnfAZlP")
